124f518d0bc940a2e16b8ea8b67915cbda6fc40e
Praise <142599118+PraiseTheNewSun@users.noreply.github.com>
2025-03-08 16:22:45 +0100

    Update: Removed Redundant Widgets And Styling

--- chat.py
+++ chat.py
@@ -75,10 +75,6 @@ class ChatApp(QMainWindow):
                     margin: 90px 90px;
                     border-image: url("img.jpg");
                 }
-                #users{
-                    width: 500px;
-                    margin-right: 500px;
-                }
                 #u_label{
                     background-color: #fff;
                     border: none;
